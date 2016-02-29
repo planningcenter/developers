390902f03a4d7405057092c442c1106d0a633aaf
Tim Morgan <tim@timmorgan.org>
2016-02-29 15:24:21 -0600

    Remove old v1 API examples

--- examples/php-pecl/me.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once("oauth_config.php");
-
-  $oauth->fetch("https://planningcenteronline.com/me.json");
-  $person = json_decode($oauth->getLastResponse());
-
-  printf("Hello $person->first_name");
-
-?>
--- examples/php-pecl/oauth_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-  define("CONSUMER_KEY",    "YOUR_CONSUMER_KEY");
-  define("CONSUMER_SECRET", "YOUR_CONSUMER_SECRET");
-  define("CALLBACK_URL",    "YOUR_CALLBACK_URL");
-
-  // Don't change this. This needs to run at the beginning of every request.
-  $oauth = new OAuth(CONSUMER_KEY, CONSUMER_SECRET);
-  $oauth->setToken($_COOKIE["oauth_token"], $_COOKIE["oauth_token_secret"]);
-?>
